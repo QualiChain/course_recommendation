1eb85f69367c6edbe3d9cc3b3fecc85cf63256bc
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-05-15 16:45:30 +0300

    Some changes in settings

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,11 @@
 import os
 
+# PostgreSQL Settings
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "admin")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "admin")
 POSTGRES_DB = os.environ.get("POSTGRES_DB", "api_db")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "qualichain.epu.ntua.gr")
+
+# Analeyezer Settings
+ANALEYEZER_HOST = os.environ.get("ANALEYEZER_HOST", "qualichain.epu.ntua.gr")
+ANALEYEZER_PORT = os.environ.get("ANALEYEZER_PORT", 5002)
