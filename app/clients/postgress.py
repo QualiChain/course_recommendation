3878b426473086cf4c3c67a1b638ac7f89c139a7
epu_temp_steve <sskalidakis@epu.ntua.gr>
2020-05-15 19:44:07 +0300

    initialisation join tables skills courses

--- app/clients/postgress.py
+++ app/clients/postgress.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
 from settings import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB
-from clients.models import Base, JoinedTableSkillsCourses, Skill, Course, Skill_Course
 
 
 class PostgresClient(object):
@@ -19,27 +18,22 @@ class PostgresClient(object):
         self.conn = self.engine.connect()
         self.session = sessionmaker(bind=self.engine)()
 
-    def initialize_tables(self):
-        # create Tables from Models
-        Base.metadata.create_all(self.engine)
-        print("Joined table Skills and Courses created successfully")
+    def load_tables(self):
+        courses_df = pd.read_sql_table('curriculum_designer_course', self.engine)
+        course_skill_df = pd.read_sql_table('curriculum_designer_course_course_skills', self.engine)
+        skill_df = pd.read_sql_table('curriculum_designer_skill', self.engine)
+        return courses_df, course_skill_df, skill_df
 
     def join_skills_and_courses(self):
-        res = self.session.query(Course.id, Course.course_title, Course.course_description, Skill.id,
-                                 Skill.skill_title).select_from(Course).join(Skill_Course).join(Skill).all()
-        return res
+        print("Joining tables Skills and Courses")
+        courses_df, course_skill_df, skill_df = self.load_tables()
+        temp = pd.merge(courses_df, course_skill_df, left_on='id', right_on='course_id')
+        joined_table = pd.merge(temp, skill_df, left_on='skill_id', right_on='id')
+        joined_table = joined_table[['id', 'course_title', 'course_description', 'skill_id', 'skill_title']].rename(
+            columns={'id': 'course_id'})
+        return joined_table
 
-    def create_joined_table_skills_courses(self):
-        results = self.join_skills_and_courses()
-
-        i = 0
-        for row in results:
-            record = JoinedTableSkillsCourses(id=i, course_id=row[0], course_title=row[1],
-                                              course_description=row[2], skill_id=row[3],
-                                              skill_title=row[4])
-            self.session.add(record)
-            self.session.commit()
-            i = i + 1
-
-    # def group_by_variable(self, agg_function, variable):
-    #     res = self.session.query()
+    def load_joined_table_to_db(self):
+        print("Uploading joined table to Postgres")
+        joined_table = self.join_skills_and_courses()
+        joined_table.to_sql('skills_courses_table', con=self.engine)
--- app/join_tables_skill_course.py
+++ app/join_tables_skill_course.py
@@ -3,5 +3,4 @@ from clients.postgress import *
 
 if __name__ == "__main__":
     post_client = PostgresClient()
-    post_client.initialize_tables()
-    # post_client.create_joined_table_skills_courses()
+    post_client.load_joined_table_to_db()
