e1f0677d75bad779093e01c3dfadedf2c3ae5e1f
vkarakolis <vkarakolis@epu.ntua.gr>
2020-05-18 14:15:14 +0300

    Added personalised skills recommendation based on clustering and an API

---
+++ app.py
@@ -0,0 +1,31 @@
+from services.cluster_data_service import  RecommenderService
+from flask_restful import Resource, Api
+from flask import request, Flask
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+
+
+class PersonalisedSkillsRecommendation(Resource):
+
+    def post(self):
+        """
+        Create a new User
+        """
+        data = request.get_json()
+        user_skills = data['Skills']
+
+        skill_names = []
+        for s in user_skills:
+            skill_names.append(s['SkillLabel'])
+
+        recommender_service = RecommenderService(skill_names)
+        recommended_skills = recommender_service.get_recommended_skills()
+        return recommended_skills, 201
+
+
+api.add_resource(PersonalisedSkillsRecommendation, '/skills_recommendation')
---
+++ services/cluster_data_service.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import settings
+import numpy as np
+import json
+
+from scipy import spatial
+
+
+class ClusterDataService(object):
+    """This Class is used to take job posts and feed them to Dobie"""
+
+    def __init__(self):
+        self.engine = create_engine(settings.ENGINE_STRING)
+
+    def get_clustering_skills(self):
+        """
+        This function is used to get job_posts table from DB
+
+        :return: job_posts table
+        """
+        select_query = 'select * from clustering_skills;'
+
+        skills = pd.read_sql_query(select_query, self.engine)
+        return skills
+
+    @staticmethod
+    def get_centroids_array():
+        return np.load('cluster_data/software_job_posts_centroids.npy')
+
+    @staticmethod
+    def get_centroid_skills_dict():
+        with open('cluster_data/idx_to_skill_dict.json', 'r') as f1:
+            idx_to_skills_dict = json.load(f1)
+        with open('cluster_data/skill_to_idx_dict.json', 'r') as f2:
+            skills_to_idx_dict = json.load(f2)
+        return idx_to_skills_dict, skills_to_idx_dict\
+
+    @staticmethod
+    def get_cluster_labels_dict():
+        with open('cluster_data/centroid_labels.json', 'r') as f2:
+            centroid_labels = json.load(f2)
+        return centroid_labels
+
+    @staticmethod
+    def create_skills_vector_from_list_of_skills(skills, skills_to_idx_dict, num_of_skills):
+        x_pred = np.zeros(num_of_skills)
+
+        for s in skills:
+            skl_idx = skills_to_idx_dict[s]
+            x_pred[skl_idx] = 1
+        return x_pred
+
+
+class RecommenderService(object):
+
+    def __init__(self, skills):
+        self.skills = skills
+        skill_extractor = ClusterDataService()
+        self.clustering_skills = skill_extractor.get_clustering_skills()
+        self.centroids_array = skill_extractor.get_centroids_array()
+        self.idx_to_skills_dict, self.skills_to_idx_dict = skill_extractor.get_centroid_skills_dict()
+        self.num_of_skills = self.centroids_array.shape[1]
+        self.user_skills_vector = skill_extractor.create_skills_vector_from_list_of_skills(self.skills, self.skills_to_idx_dict, self.num_of_skills)
+        self.closer_clusters = self.get_closer_clusters()
+        self.centroid_labels = skill_extractor.get_cluster_labels_dict()
+
+    def get_recommended_skills(self):
+        skills_per_cluster = {}
+        for c in self.closer_clusters:
+            skills_of_c = []
+            for sk in np.argsort(-self.centroids_array[c])[0:20]:
+                if self.centroids_array[c][sk] <= 0.25:
+                    break
+                if self.user_skills_vector[sk] == 1:
+                    continue
+                skills_of_c.append(self.idx_to_skills_dict[str(sk)])
+            skills_per_cluster[self.centroid_labels[str(c)]] = skills_of_c
+
+        return skills_per_cluster
+
+    def get_closer_clusters(self):
+        distance_vec = np.zeros(11)
+
+        i = 0
+        for c in self.centroids_array:
+            distance_vec[i] = spatial.distance.cosine(c, self.user_skills_vector)
+            i += 1
+
+        return np.argsort(distance_vec)[0:3]
+
