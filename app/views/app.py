c2437235f6b8975f51697a6b41de5599efd9d69a
vkarakolis <vkarakolis@epu.ntua.gr>
2020-05-23 12:16:57 +0300

    added courses recommendation based on clustering and elk

--- app/views/app.py
+++ app/views/app.py
@@ -4,6 +4,15 @@ import sys
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+from services.cluster_data_service import RecommenderService
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+CORS(app)
+
+api = Api(app)
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 log = logging.getLogger(__name__)
@@ -36,3 +45,32 @@ def get_skills_from_cv():
     results = {'status': status, 'results': results}
 
     return jsonify(results)
+
+
+@app.route('/get_recommended_skills', methods=['POST'])
+def get_recommended_skills():
+    data = request.get_json()
+    user_skills = data['Skills']
+
+    skill_names = []
+    for s in user_skills:
+        skill_names.append(s['SkillLabel'])
+
+    recommender_service = RecommenderService(skill_names)
+    recommended_skills = recommender_service.get_recommended_skills()
+    return jsonify(recommended_skills)
+
+
+@app.route('/get_recommended_courses', methods=['POST'])
+def get_recommended_courses():
+    data = request.get_json()
+    user_skills = data['Skills']
+
+    skill_names = []
+    for s in user_skills:
+        skill_names.append(s['SkillLabel'])
+
+    recommender_service = RecommenderService(skill_names)
+    recommended_courses = recommender_service.get_recommended_courses()
+    return jsonify(recommended_courses)
+
